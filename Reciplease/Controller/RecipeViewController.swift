3f56b80bdcf17854519153e79c220eeb110f01a8
Laurent DEBEAUJON <Laurent.DEBEAUJON@gmail.com>
2020-11-08 18:35:50 +0100

    storyboard 0.5

---
+++ Reciplease/Controller/RecipeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RecipeViewController.swift
+//  Reciplease
+//
+//  Created by Laurent Debeaujon on 08/11/2020.
+//
+
+import UIKit
+
+class RecipeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
